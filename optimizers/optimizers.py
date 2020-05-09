c99256ce5abf31816df6541aceb8713dc5af2c19
sanfernoronha <sanfernoronha99@gmail.com>
2020-05-09 18:38:54 +0530

    Started rough work on momentum

--- optimizers/optimizers.py
+++ optimizers/optimizers.py
@@ -6,8 +6,18 @@ def GradientDescent(w, grad, learning_rate):
     return w_updated
 
 
-def Momentum(w, grad, learning_rate, beta):
-    pass
+def Momentum(w, grad, learning_rate, beta,v): #pass v as an initialized vector
+    v = beta * v + (1 - beta) * grad
+    w_prev = w #keep track of parameters of the previous iteration
+    w_updated = w - learning_rate * v
+    print("Value of weights at this iteration ", w_updated)
+    if w_prev == w_updated:
+        return ("done",w_updated)
+    else:
+        return v,w_updated #return new weights and the v vector to pass it back again for next iteration
+
+        
+
 
 
 def RMSProp(w, grad, learning_rate, beta, epsilon):
