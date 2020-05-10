9800e9953d6e047bca42253e316b0ee7def66eb5
Ryan Dsilva <ryan.dsilva.98@gmail.com>
2020-05-10 13:20:34 +0530

    cleanup optimizers

--- optimizers/optimizer_functions.py
+++ optimizers/optimizer_functions.py
@@ -21,21 +21,13 @@ def GradientDescent(w, b, dW, dB, learning_rate=0.01):
     return w_updated, b_updated
 
 
-def Momentum(w, grad, learning_rate, beta, v):  # pass v as an initialized vector
-    v = beta * v + (1 - beta) * grad
-    w_prev = w  # keep track of parameters of the previous iteration
-    w_updated = w - learning_rate * v
-    print("Value of weights at this iteration ", w_updated)
-    if w_prev == w_updated:
-        return ("done", w_updated)
-    else:
-        # return new weights and the v vector to pass it back again for next iteration
-        return v, w_updated
-
-
-def RMSProp(w, grad, learning_rate, beta, epsilon):
+def Momentum(w, b, dW, dB, learning_rate, beta):
     pass
 
 
-def Adam(w, grad, learning_rate, beta1, beta2, epsilon):
+def RMSProp(w, b, dW, dB, learning_rate, beta, epsilon):
+    pass
+
+
+def Adam(w, b, dW, dB, learning_rate, beta1, beta2, epsilon):
     pass
