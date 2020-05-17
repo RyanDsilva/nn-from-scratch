26b03ec90a42cbe79e8955aea4b57992d70dc4f0
Ryan Dsilva <ryan.dsilva.98@gmail.com>
2020-05-17 15:08:46 +0530

    standardized variable notations

--- core/dense.py
+++ core/dense.py
@@ -9,17 +9,18 @@ class Dense(Layer):
     def __init__(self, input_size, output_size):
         self.weights = np.random.rand(input_size, output_size) - 0.5
         self.bias = np.random.rand(1, output_size) - 0.5
-        self.v_weights = (
-            np.zeros((input_size, output_size)) + 0.0
-        )  # state initialization for every layer
-        self.v_bias = (
-            np.zeros((1, output_size)) + 0.0
-        )  # state initialization for every layer
+        self.vW = np.zeros([input_size, output_size]) + 0.0
+        # (
+        #     np.zeros((input_size, output_size)) + 0.0
+        # )  # state initialization for every layer
+        self.vB = np.zeros([1, output_size]) + 0.0
+        # (
+        #     np.zeros((1, output_size)) + 0.0
+        # )  # state initialization for every layer
 
     def forward_propagation(self, input_data):
         self.input = input_data
         self.output = np.dot(self.input, self.weights) + self.bias
-
         return self.output
 
     def backward_propagation(self, output_error, optimizer_fn, learning_rate):
@@ -27,15 +28,11 @@ class Dense(Layer):
         dW = np.dot(self.input.T, output_error)
         dB = output_error
 
-        w_updated, b_updated, vw_updated, vb_updated = optimizer_fn.minimize(
-            self.weights, self.bias, dW, dB, self.v_weights, self.v_bias, learning_rate
+        w_updated, b_updated, vW_updated, vB_updated = optimizer_fn.minimize(
+            self.weights, self.bias, dW, dB, self.vW, self.vB, learning_rate
         )
-
-        ### updates
         self.weights = w_updated
-
         self.bias = b_updated
-
-        self.v_weights = vw_updated
-        self.v_bias = vb_updated
+        self.vW = vW_updated
+        self.vB = vB_updated
         return input_error
--- optimizers/optimizer_functions.py
+++ optimizers/optimizer_functions.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 class GradientDescent:
-    def minimize(self, w, b, dW, dB, vw, vb, learning_rate=0.01, beta=0.9):
+    def minimize(self, w, b, dW, dB, vW, vB, learning_rate=0.01):
         """Implements Gradient Descent to find minima of cost function
 
     Parameters:
@@ -19,11 +19,11 @@ class GradientDescent:
     """
         w_updated = w - learning_rate * dW
         b_updated = b - learning_rate * dB
-        return w_updated, b_updated, vw, vb
+        return w_updated, b_updated, vW, vB
 
 
 class Momentum:
-    def minimize(self, w, b, dW, dB, vw, vb, learning_rate=0.01, beta=0.9):
+    def minimize(self, w, b, dW, dB, vW, vB, learning_rate=0.01, beta=0.9):
         """Implements Gradient Descent with Momentum to find minima of cost function
 
         Parameters:
@@ -32,26 +32,23 @@ class Momentum:
         - dW (numpy array): gradient of weights matrix wrt cost function
         - dB (numpy array): gradient of bias matrix wrt cost function
         - learning_rate (double): learning rate used to update weights
-        - beta (double):
-        - vw (numpy array): holds the state of the optimizer for previous iteration (weights)
-        - vb (numpy array): holds the state of the optimizer for previous iterations (biases)
+        - beta (double): Momentum term for smoothing
+        - vW (numpy array): holds the state of the optimizer for previous iteration (weights)
+        - vB (numpy array): holds the state of the optimizer for previous iterations (biases)
 
         Returns:
         - w_updated (numpy array): updated weights
         - b_updated (numpy array): updated bias
-        - vw (numpy array): updated state of the optimizer for current iteration (weights)
-        - vb (numpy array): updated state of the optimizer for current iteration (biases)
+        - vW (numpy array): updated state of the optimizer for current iteration (weights)
+        - vB (numpy array): updated state of the optimizer for current iteration (biases)
 
         """
 
-        vw = beta * vw + (1 - beta) * dW
-
-        vb = beta * vb + (1 - beta) * dB
-
-        w_updated = w - learning_rate * vw
-        b_updated = b - learning_rate * vb
-
-        return w_updated, b_updated, vw, vb
+        vW = beta * vW + (1 - beta) * dW
+        vB = beta * vB + (1 - beta) * dB
+        w_updated = w - learning_rate * vW
+        b_updated = b - learning_rate * vB
+        return w_updated, b_updated, vW, vB
 
 
 def RMSProp(w, b, dW, dB, learning_rate, beta, epsilon):
