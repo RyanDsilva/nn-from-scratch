a4f459badc03824d05045fe6c88df9e1f750316b
vishalmanes109 <36571896+vishalmanes109@users.noreply.github.com>
2020-10-08 22:56:45 +0530

    cross entropy function added

--- loss/loss_functions.py
+++ loss/loss_functions.py
@@ -15,3 +15,16 @@ def MAE(y, yhat):
 
 def dMAE(y, yhat):
     return 1 if y == yhat else -1
+
+
+def kl_divergence(y, yhat):
+	return sum(y[i] * log2(y[i]/yhat[i]) for i in range(len(y)))
+
+def entropy(y,ets=1e-15):
+	return -sum([y[i] * log2(y[i]+ets) for i in range(len(y))])
+
+
+def cross_entropy(y,yhat,mode=None,,ets=1e-15):
+    if(mode=='Kl_diversion'):
+        return entropy(y) + kl_divergence(y, yhat)
+	return -sum([y[i]*log2(yhat[i]+ets) for i in range(len(y))])
